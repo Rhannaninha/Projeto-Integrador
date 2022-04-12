f7534fa0c9fbe88f70263dc34ccf7897f922fc56
Rhannaninha <95387010+Rhannaninha@users.noreply.github.com>
2022-04-12 12:16:33 -0300

    Add files via upload

---
+++ Projeto 2#/Dados.java
@@ -0,0 +1,51 @@
+package br.senai.model;
+
+public class Dados {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+    private String pagamento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(String pagamento) {
+        this.pagamento = pagamento;
+    }
+    
+}
---
+++ Projeto 2#/Main.java
@@ -0,0 +1,13 @@
+package br.senai;
+
+import br.senai.model.Dados;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dados dados1 = new Dados();
+
+    }
+
+}
